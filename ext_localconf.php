340f42c5859da9467f2c261a66a0c0f2d3eadffa
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2025-07-25 18:23:54 +0200

    Added option to modify iconpack styles

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,6 +9,7 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('iconpack')) {
         'EXT:iconpack_linea/Configuration/Iconpack/Linea-1.0.yaml',
         \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
             \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
-        )->get('iconpack_linea', 'configFile')
+        )->get('iconpack_linea', 'configFile'),
+        \Quellenform\IconpackLinea\Configuration\IconpackConfiguration::class
     );
 }
